5a53c49532aa05acb228a866255087221faf15f5
LwaziMgazi <34287957+LwaziMgazi@users.noreply.github.com>
2023-01-01 09:02:50 -0800

    adding email api changes

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { env } from 'process';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   app.enableCors();
   await app.listen(process.env.PORT || 3000);
 }
