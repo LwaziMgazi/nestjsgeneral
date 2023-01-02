d24961d51230fad824fea904eb3932ab83ac4fe5
LwaziMgazi <34287957+LwaziMgazi@users.noreply.github.com>
2023-01-01 19:27:56 -0800

    adding email api changes02

--- src/main.ts
+++ src/main.ts
@@ -3,18 +3,14 @@ import { env } from 'process';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule);
 
-  app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
-    next();
-});
-  app.enableCors({
-    allowedHeaders:"*",
-        origin: "*"
-});
+
+  app.enableCors(
+    { 
+      origin: ['https://pensdowns.web.app/'],
+    }
+);
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
